cab14473f9854f25270b41d28a69074052eaf70e
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-05-15 22:54:47 +0900

    fix whitespace

--- lib/yakiudon/config.rb
+++ lib/yakiudon/config.rb
@@ -1,30 +1,30 @@
 require 'yaml'
 
 module Yakiudon
-	module Config
-		class << self
-			def read
-				@@yaml ||= {"public"      => "#{File.dirname(__FILE__)}/../../public",
-										"template"    => "#{File.dirname(__FILE__)}/../../template",
-										"db"          => "#{File.dirname(__FILE__)}/../../db",
-										"user"        => "yaki",
-										"password"    => "udon",
-										"recent"      => 10,
-										"url"         => "/",
-				            "title"       => "Untitled Yakiudon",
-				            "description" => "With some chunky bacons.",
-				            "head_shift"  => 3}
-				@@yaml.merge!(YAML.load_file(CONFIG)) if File.exist?(CONFIG)
-				@@yaml["url"].sub!(/\/$/,"")
-				@@yaml
-			end
+  module Config
+    class << self
+      def read
+        @@yaml ||= {"public"      => "#{File.dirname(__FILE__)}/../../public",
+                    "template"    => "#{File.dirname(__FILE__)}/../../template",
+                    "db"          => "#{File.dirname(__FILE__)}/../../db",
+                    "user"        => "yaki",
+                    "password"    => "udon",
+                    "recent"      => 10,
+                    "url"         => "/",
+                    "title"       => "Untitled Yakiudon",
+                    "description" => "With some chunky bacons.",
+                    "head_shift"  => 3}
+        @@yaml.merge!(YAML.load_file(CONFIG)) if File.exist?(CONFIG)
+        @@yaml["url"].sub!(/\/$/,"")
+        @@yaml
+      end
 
-			def method_missing(name,*args)
-				self.read[name.to_s]
-			end
+      def method_missing(name,*args)
+        self.read[name.to_s]
+      end
 
-			def yaml; @@yaml; end
-			def yaml=(x); @@yaml = x; end
-		end
-	end
+      def yaml; @@yaml; end
+      def yaml=(x); @@yaml = x; end
+    end
+  end
 end
--- lib/yakiudon/editor.rb
+++ lib/yakiudon/editor.rb
@@ -17,41 +17,41 @@ helpers do
 end
 
 get "/edit/:id" do
-	protected!
-	id = params[:id].gsub(/[^\d]/,"")
-	@day = Yakiudon::Model::Day.new(id)
-	erb :edit
+  protected!
+  id = params[:id].gsub(/[^\d]/,"")
+  @day = Yakiudon::Model::Day.new(id)
+  erb :edit
 end
 
 get "/edit" do
-	redirect "#{Yakiudon::Config.url}/edit/#{Time.now.strftime("%Y%m%d")}"
+  redirect "#{Yakiudon::Config.url}/edit/#{Time.now.strftime("%Y%m%d")}"
 end
 
 post "/edit/:id" do
-	protected!
-	id = params[:id].gsub(/[^\d]/,"")
-	return "invalid id" unless id.size == 8
+  protected!
+  id = params[:id].gsub(/[^\d]/,"")
+  return "invalid id" unless id.size == 8
   day = Yakiudon::Model::Day.new(id)
-	if params[:a] == "Delete"
-		day.delete
-		redirect "#{Yakiudon::Config.url}/index.html"
-	else
-		day.markdown = params[:body]
-		day.meta["title"] = params[:title]
-		day.save
+  if params[:a] == "Delete"
+    day.delete
+    redirect "#{Yakiudon::Config.url}/index.html"
+  else
+    day.markdown = params[:body]
+    day.meta["title"] = params[:title]
+    day.save
 
-		Yakiudon::HTML.renew_index
-		Yakiudon::Model::Meta.save
-		Yakiudon::HTML.build_includes(id)
+    Yakiudon::HTML.renew_index
+    Yakiudon::Model::Meta.save
+    Yakiudon::HTML.build_includes(id)
 
-		redirect "#{Yakiudon::Config.url}/#{id}.html"
-	end
+    redirect "#{Yakiudon::Config.url}/#{id}.html"
+  end
 end
 
 post "/build" do
-	protected!
-	Yakiudon::HTML.renew_index
+  protected!
+  Yakiudon::HTML.renew_index
   Yakiudon::Model::Meta.save
-	Yakiudon::HTML.build_all
-	redirect "#{Yakiudon::Config.url}/index.html"
+  Yakiudon::HTML.build_all
+  redirect "#{Yakiudon::Config.url}/index.html"
 end
